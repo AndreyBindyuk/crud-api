dc0d4acc411caecc8505bdbb79d51e01105ab50d
andrey.bindyuk <andrey.bindyuk@aliexpress.ru>
2024-10-22 01:33:11 +0500

    feat: impl put,delete, and get all users and user by id

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4, validate as uuidValidate } from 'uuid';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -8,6 +8,23 @@ const app = express();
 app.use(express.json());
 
 const users: Record<string, any> = {};
+
+app.get('/api/users', (req, res) => {
+  res.status(200).json(Object.values(users));
+});
+
+// GET user by ID
+app.get('/api/users/:userId', (req, res) => {
+  const { userId } = req.params;
+  if (!uuidValidate(userId)) {
+    res.status(400).send('Invalid UUID');
+  }
+  const user = users[userId];
+  if (!user) {
+    res.status(404).send('User not found');
+  }
+  res.status(200).json(user);
+});
 
 app.post('/api/users', (req, res) => {
   const { age, hobbies } = req.body;
@@ -18,6 +35,38 @@ app.post('/api/users', (req, res) => {
   const newUser = { id, age, hobbies };
   users[id] = newUser;
   res.status(201).json(newUser);
+});
+
+app.put('api/users/:userId', (req, res) => {
+  const { userId } = req.params;
+  if (!uuidValidate(userId)) {
+    res.status(400).send('Invalid UUID');
+  }
+  const user = users[userId];
+  if (!user) {
+    res.status(404).send('User not found');
+  }
+  const { age, hobbies } = req.body;
+  if (typeof age !== 'number' || !Array.isArray(hobbies)) {
+    res.status(400).send('Invalid request body');
+  }
+
+  user.age = age;
+  user.hobbies = hobbies;
+  res.status(200).json(user);
+});
+
+app.delete('api/user/:userId', (req, res) => {
+  const { userId } = req.params;
+  if (!uuidValidate(userId)) {
+    res.status(400).send('Invalid UUID');
+  }
+  const user = users[userId];
+  if (!user) {
+    res.status(404).send('User not found');
+  }
+  delete users[userId];
+  res.status(204).send();
 });
 
 const PORT = process.env.PORT || 3000;
